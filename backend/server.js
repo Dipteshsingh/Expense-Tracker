54c95eb5ceedf4dde21ceb1c8aba81c35dabb28e
Dipteshsingh <diptesh.singh73@gmail.com>
2026-04-17 01:15:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://expense-tracker-frontend-zipm.onrender.com",
     credentials: true,
   })
 );
