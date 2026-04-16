bb812853a3d06458161a6266be61a6d51873263b
Dipteshsingh <diptesh.singh73@gmail.com>
2026-04-17 01:57:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://expense-tracker-frontend1-9fax.onrender.com",
+    origin: "*",
     credentials: true,
   })
 );
